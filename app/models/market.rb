042c907005603cb7f1e352cb4d6cc557f3778542
Vinh Bui <bicyclethief@gmail.com>
2015-04-03 16:10:20 -0400

    fix dogs database being created

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,28 +1,28 @@
-require 'openssl'
-require 'geokit'
+# require 'openssl'
+# require 'geokit'
 
 class Market < ActiveRecord::Base
-  belongs_to :geodata
+  # belongs_to :geodata
 
   # TODO: validate data
 
-  def nearest_market(zipcode, num)
-    distance_markets = {}
+  # def nearest_market(zipcode, num)
+  #   distance_markets = {}
 
-    user_geodata = Geokit::Geocoders::GoogleGeocoder.geocode(zipcode)
-    Market.all.each do |market|
-      city_state = "#{market.borough, market.state_code}"
-      full_address = "#{market.street_address} #{city_state}"
-      market_geodata = Geokit::Geocoders::GoogleGeocoder.geocode(full_address)
-      distance = user_geodata.distance_to(market_geodata)
-      distance_markets[distance] = market
-    end
+  #   user_geodata = Geokit::Geocoders::GoogleGeocoder.geocode(zipcode)
+  #   Market.all.each do |market|
+  #     city_state = "#{market.borough, market.state_code}"
+  #     full_address = "#{market.street_address} #{city_state}"
+  #     market_geodata = Geokit::Geocoders::GoogleGeocoder.geocode(full_address)
+  #     distance = user_geodata.distance_to(market_geodata)
+  #     distance_markets[distance] = market
+  #   end
 
-    closest_distances = distance_markets.keys.sort.first(num)
+  #   closest_distances = distance_markets.keys.sort.first(num)
 
-    closest_distances.map do |key|
-      distance_markets[key]
-    end
-  end
+  #   closest_distances.map do |key|
+  #     distance_markets[key]
+  #   end
+  # end
 
 end
