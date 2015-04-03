78650c9e33b2754b9e53a22d1684fc0633ac596a
Charles Green <Charles.Green88@gmail.com>
2015-04-03 17:02:23 -0400

    controller framework implemented

--- markets.rb
+++ markets.rb
@@ -1,32 +1,42 @@
 
 class Controller
-  def initialize
-
-  end
-
-  # case statement?
 
   def run(command)
-    display(query(command))
+    query(command).each do |market|
+      display(market)
+    end
   end
 
   def query(zip_code_input)
     nearest_markets = []
-    # use geodata to return 3 nearest markets
+    # use geokit to return 3 nearest markets
     return nearest_markets
   end
 
-  def display(market)
+  def display
+
     puts "Nearest farmer's markets:"
-    puts "1.) #{market.market_name}"
-    puts "#{market.full_address}"
-    puts "#{market.borough}, NY, #{market.zip}"
-    puts "Open: #{market.days_s} #{market.hours}"
+    puts
+    puts "Bissel Gardens Farmers' Market"
+    puts "--------"
+    puts "Baychester Ave & E 241st St, Bronx, NY"
+    puts "Bronx, NY, 11207"
+    puts "Open: Wednesday & Saturday, 9am-5pm"
   end
 
+
+  # def display
+  #   puts "Nearest farmer's markets:"
+  #   puts
+  #   puts "#{market.market_name}"
+  #   puts "----------"
+  #   puts "#{market.full_address}"
+  #   puts "#{market.borough}, NY, #{market.zip}"
+  #   puts "Open: #{market.days_s} #{market.hours}"
+  # end
 end
 
 command = ARGV[0]
 
 market_finder = Controller.new
-market_finder.run(command)
+market_finder.display
