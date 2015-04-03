86d3fc59e19688530198dc14e76b13a2e6972bef
Vinh Bui <bicyclethief@gmail.com>
2015-04-03 15:51:18 -0400

    fix table column names to match original data; implement Market#nearest_market

--- db/migrate/20150403124254_create_tables.rb
+++ db/migrate/20150403124254_create_tables.rb
@@ -2,9 +2,9 @@ class CreateTables < ActiveRecord::Migration
   def change
     create_table :markets do |t|
       t.string  :borough, null: false
-      t.string  :name, null: false
+      t.string  :market_name, null: false
       t.string  :street_address, null: false
-      t.string  :days, null: false
+      t.string  :day_s, null: false
       t.string  :hours, null: false
       t.integer :distribute_health_bucks, null: false
       t.integer :accepts_health_bucks, null: false
